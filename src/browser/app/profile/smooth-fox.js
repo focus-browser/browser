7030ad3a9f4dc7e4f4d11a76ede26e151f805559
fxcl <74125308+fxcl@users.noreply.github.com>
2024-04-04 12:02:17 +0800

    .

--- src/browser/app/profile/smooth-fox.js
+++ src/browser/app/profile/smooth-fox.js
@@ -1,8 +1,8 @@
-// The code in this file was created by @yokoffing and is licensed under the
-// MIT license:
-// MIT License
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// Copyright (c) 2020 yokoffing
+// Note: You must have semicolons at the end of each line in user setting files
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
